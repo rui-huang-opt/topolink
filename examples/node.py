6c07ba2a143dcc86814648b43821c968f80a4e03
Rui Huang <ruihuang0421@gmail.com>
2025-06-18 18:53:35 +0100

    将节点个数扩展到了五个，此外检查了算法是否同步。

--- examples/node.py
+++ examples/node.py
@@ -1,6 +1,31 @@
 import sys
+import numpy as np
 from numpy.random import seed, uniform
 from topolink import NodeHandle
+
+
+def sync_check(node_handle: NodeHandle) -> None:
+    for k in range(10):
+        node_handle.broadcast(np.array(k, dtype=np.float64))
+        iter_arr = np.hstack(node_handle.gather())
+
+        sync = np.all(iter_arr == k)
+
+        print(
+            f"Node {node_name} at iteration {k}: "
+            f"gathered states: {iter_arr}, sync: {sync}"
+        )
+
+
+def gossip_check(node_handle: NodeHandle) -> None:
+    state = uniform(-100.0, 100.0, size=3)
+
+    for _ in range(100):
+        laplacian = node_handle.compute_laplacian(state)
+        state -= laplacian * 0.45
+
+        print(f"Node {node_name} updated state: {state}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -12,12 +37,7 @@ if __name__ == "__main__":
     node_name = sys.argv[1]
     nh = NodeHandle(node_name, server_address="localhost:5555")
 
-    state = uniform(-100.0, 100.0, size=3)
-
     input("Press Enter to start the node...")
 
-    for _ in range(100):
-        laplacian = nh.compute_laplacian(state)
-        state -= laplacian * 0.45
-
-        print(f"Node {node_name} updated state: {state}")
+    sync_check(nh)
+    # gossip_check(nh)
--- examples/server.py
+++ examples/server.py
@@ -1,7 +1,7 @@
 from topolink import Topology
 
 if __name__ == "__main__":
-    node_names = ["1", "2", "3"]
-    edge_pairs = [("1", "2"), ("2", "3"), ("3", "1")]
+    node_names = ["1", "2", "3", "4", "5"]
+    edge_pairs = [("1", "2"), ("2", "3"), ("3", "4"), ("4", "5"), ("5", "1")]
     topology = Topology(node_names, edge_pairs, address="localhost:5555")
     topology.deploy()
