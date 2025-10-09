f2afeaa734ab8ca86bd58459118b7baaf15b0f5a
Rui Huang <ruihuang0421@gmail.com>
2025-10-09 12:49:11 +0100

    修改node测试脚本。

--- examples/scripts/node.py
+++ examples/scripts/node.py
@@ -1,35 +1,30 @@
 import sys
 from logging import basicConfig, INFO
-from topolink import NodeHandle
-
-
-def consensus(node_handle: NodeHandle, n_state: int = 3, n_iter: int = 100) -> None:
-    from numpy import zeros
-    from numpy.random import seed, uniform
-
-    name = node_handle.name
-
-    states = zeros((n_iter, n_state))
-    seed(int(name))  # Ensure reproducibility for each node
-    states[0] = uniform(-100.0, 100.0, n_state)
-
-    print(f"Node {name} initial state: {states[0]}")
-
-    for k in range(n_iter - 1):
-        lap_state = nh.laplacian(states[k])
-        states[k + 1] = states[k] - 0.45 * lap_state
-
-        print(f"Node {name} at iteration {k + 1}: state: {states[k + 1]}")
-
 
 basicConfig(level=INFO)
 
 if len(sys.argv) > 1:
-    node_name = "".join(sys.argv[1:])
+    name = "".join(sys.argv[1:])
 else:
     print("Usage: python node.py <node_name>")
     sys.exit(1)
 
-nh = NodeHandle(node_name)
+from numpy import zeros
+from numpy.random import seed, uniform
+from topolink import NodeHandle
 
-consensus(nh)
+n_state = 3
+n_iter = 100
+nh = NodeHandle(name)
+
+states = zeros((n_iter, n_state))
+seed(int(name))  # Ensure reproducibility for each node
+states[0] = uniform(-100.0, 100.0, n_state)
+
+print(f"Node {name} initial state: {states[0]}")
+
+for k in range(n_iter - 1):
+    lap_state = nh.laplacian(states[k])
+    states[k + 1] = states[k] - 0.45 * lap_state
+
+    print(f"Node {name} at iteration {k + 1}: state: {states[k + 1]}")
